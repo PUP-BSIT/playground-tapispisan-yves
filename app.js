b5d508ac662485285f1bae4bc5b8298aa4f3a02a
Yves John B. Tapispisan <bancolitayves@gmail.com>
2024-03-17 15:25:19 +0800

    add activity

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("Hello World!");
+//8
+var DistanceUnit;
+(function (DistanceUnit) {
+    DistanceUnit["KILOMETER"] = "km";
+    DistanceUnit["MILE"] = "m";
+})(DistanceUnit || (DistanceUnit = {}));
+//3
+var myCar = {
+    plate_no: "ABC 1234",
+    owner_name: "John Doe",
+    speed: 50,
+    printSpeed: function () {
+        console.log("The current speed of ".concat(this.plate_no, " is ").concat(this.speed, " ").concat(DistanceUnit.MILE));
+    }
+};
+//4
+var janeDee = {
+    full_name: "Jane Dee",
+    birthday: new Date("2002-03-25"),
+    speed: 6.5,
+    printSpeed: function () {
+        console.log("The current speed of ".concat(this.full_name, " is ").concat(this.speed, " ").concat(DistanceUnit.KILOMETER));
+    }
+};
+//5
+function speedTest(contestant) {
+    //6
+    console.log("Speed Test On Going...");
+    contestant.printSpeed();
+}
+//7
+speedTest(janeDee);
+speedTest(myCar);
--- app.ts
+++ app.ts
@@ -1,1 +1,56 @@
-console.log("Hello World!");
+//8
+enum DistanceUnit {
+    KILOMETER = "km",
+    MILE = "m"
+}
+
+//1
+type Vehicle = {
+    plate_no: string;
+    owner_name: string;
+    speed: number; 
+    printSpeed(): void;
+};
+
+//3
+const myCar: Vehicle = {
+    plate_no: "ABC 1234",
+    owner_name: "John Doe",
+    speed: 50,
+    printSpeed() {
+        console.log(`The current speed of ${this.plate_no} is ${this.speed} ${DistanceUnit.MILE}`);
+    }
+};
+
+//2
+type Person = {
+    full_name: string;
+    birthday: Date;
+    speed: number; 
+    printSpeed(): void;
+};
+
+//4
+const janeDee: Person = {
+    full_name: "Jane Dee",
+    birthday: new Date("2002-03-25"),
+    speed: 6.5,
+    printSpeed() {
+        console.log(`The current speed of ${this.full_name} is ${this.speed} ${DistanceUnit.KILOMETER}`)
+    }
+}
+
+
+//9
+type Contestant = Person | Vehicle;
+//5
+function speedTest(contestant: Contestant): void {
+    //6
+    console.log("Speed Test On Going...");
+    contestant.printSpeed();
+}
+
+//7
+speedTest(janeDee);
+speedTest(myCar);
+
